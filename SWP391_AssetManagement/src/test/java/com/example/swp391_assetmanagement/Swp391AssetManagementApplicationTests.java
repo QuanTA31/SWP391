cabe0142222285d8ad31e8289e1594c495daee85
quanta <quanvamvo@gmail.com>
2026-01-27 11:08:36 +0700

    quanta : update example

--- SWP391_AssetManagement/src/test/java/com/example/swp391_assetmanagement/Swp391AssetManagementApplicationTests.java
+++ SWP391_AssetManagement/src/test/java/com/example/swp391_assetmanagement/Swp391AssetManagementApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.swp391_assetmanagement;
 
-import org.junit.jupiter.apil.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +9,4 @@ class Swp391AssetManagementApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
