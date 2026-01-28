1b430e21c640cf9e872566674a0bd4f8c7d55eb0
QuanTA <quanvamvo@gmail.com>
2026-01-28 23:26:57 +0700

    QuanTA : update cấu trúc

--- SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/service/AssetService.java
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/service/AssetService.java
@@ -1,11 +1,11 @@
 package com.example.swp391_assetmanagement.service;
 
-import com.example.swp391_assetmanagement.service.servicerequest.AssetViewAllServiceRequest;
-import com.example.swp391_assetmanagement.service.serviceresponse.AssetViewAllServiceResponse;
+import com.example.swp391_assetmanagement.service.servicerequest.AssetViewAllRequest;
+import com.example.swp391_assetmanagement.service.serviceresponse.AssetViewAllResponse;
 
 import java.util.List;
 
 public interface AssetService {
 
-    List<AssetViewAllServiceResponse> viewAllAsset(AssetViewAllServiceRequest assetRequest);
+    List<AssetViewAllResponse> viewAllAsset(AssetViewAllRequest assetRequest);
 }
