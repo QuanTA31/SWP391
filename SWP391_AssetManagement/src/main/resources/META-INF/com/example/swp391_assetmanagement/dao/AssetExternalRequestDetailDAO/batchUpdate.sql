18d0d750cf731511fff9a8e5331fcffd39f1ecef
QuanTA <quanvamvo@gmail.com>
2026-02-27 08:52:30 +0700

    QuanTA : create purchar request

--- SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/AssetExternalRequestDetailDAO/batchUpdate.sql
+++ SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/AssetExternalRequestDetailDAO/batchUpdate.sql
@@ -1,3 +1,6 @@
 UPDATE asset_external_request_detail
-SET external_status_id = /* details.externalStatusId */''
+SET external_status_id = /* details.externalStatusId */'',
+    asset_type_id = /* details.assetTypeId */'',
+    quantity = /* details.quantity */'',
+    note = /* details.note */''
 WHERE id = /* details.id */1
