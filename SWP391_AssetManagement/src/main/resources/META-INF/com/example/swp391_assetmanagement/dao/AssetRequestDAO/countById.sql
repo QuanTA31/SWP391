b9c47c9c913763be300802779cbb8663e5bbdb79
quanta <quanvamvo@gmail.com>
2026-02-28 11:29:36 +0700

    QuanTA : update condition update asset_request

--- SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/AssetRequestDAO/countById.sql
+++ SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/AssetRequestDAO/countById.sql
@@ -2,4 +2,4 @@ SELECT COUNT(1)
 FROM asset_request
 INNER JOIN asset_external_request_detail ON asset_request.id = asset_external_request_detail.asset_request_id
 WHERE asset_request.id = /* assetRequestId */1
-AND asset_request.request_status_id = /* status */''
+AND asset_request.request_status_id != /* status */''
