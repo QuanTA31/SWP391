185dd176458dfb1ab9a3f682cb9cb09f0646b581
quanta <quanvamvo@gmail.com>
2026-03-01 10:15:56 +0700

    QuanTA : create new entity

---
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/service/AssetsAssetRequestExternalService.java
@@ -0,0 +1,4 @@
+package com.example.swp391_assetmanagement.service;
+
+public interface AssetsAssetRequestExternalService {
+}
---
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/service/serviceimpl/AssetsAssetRequestExternalServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.swp391_assetmanagement.service.serviceimpl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AssetsAssetRequestExternalServiceImpl {
+}
