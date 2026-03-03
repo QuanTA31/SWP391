cb12910d2a430b5991859e654d89335fb2e0859b
Longbhnd12 <longbhnd12@gmail.com>
2026-03-03 11:32:16 +0700

    LongNT:edit st

--- SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/OptionDetailDao/selectByUpdate.sql
+++ SWP391_AssetManagement/src/main/resources/META-INF/com/example/swp391_assetmanagement/dao/OptionDetailDao/selectByUpdate.sql
@@ -2,11 +2,4 @@ SELECT
     /*%expand*/*
 FROM option_detail
 WHERE asset_external_request_detail_id = /* assetExternalRequestDetailId */0
-  AND (SELECT COUNT(1)
-       FROM option_detail
-       WHERE asset_external_request_detail_id = /* assetExternalRequestDetailId */0)
-    = (SELECT COUNT(1)
-       FROM option_detail
-       WHERE asset_external_request_detail_id = /* assetExternalRequestDetailId */0
-         AND is_selected IS NULL)
     FOR UPDATE NOWAIT
