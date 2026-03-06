c7c31b759431fc9085a34d6c0065433921f377bb
quanta <quanvamvo@gmail.com>
2026-03-06 08:56:53 +0700

    QuanTA : update logic check role

--- SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/common/RoleChecker.java
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/common/RoleChecker.java
@@ -5,9 +5,9 @@ import com.example.swp391_assetmanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ResponseStatusException;
 
-@Component
 @RequiredArgsConstructor
 public class RoleChecker {
 
@@ -16,19 +16,19 @@ public class RoleChecker {
     public void requireRole(String userCode, Roles... roles) {
 
         //check user tồn tại
-        if (userCode == null || userCode.isBlank()) {
+        if (ObjectUtils.isEmpty(userCode)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
         //lấy role id qua user code
         String roleCode = userService.getRoleCodesByUserCode(userCode);
 
         //check role null
-        if (roleCode == null) {
+        if (ObjectUtils.isEmpty(roleCode)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
         //check role tồn tại trong db
         for (Roles role : roles) {
-            if (role.name().equals(roleCode)) {
+            if (Roles.of(roleCode).equals(role)) {
                 return;
             }
         }
