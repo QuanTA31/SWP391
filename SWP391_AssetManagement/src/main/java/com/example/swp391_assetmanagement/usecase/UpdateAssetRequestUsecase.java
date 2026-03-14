8e17e535dd005576569a742b95ca64ad358b4df6
Longbhnd12 <longbhnd12@gmail.com>
2026-03-14 08:58:56 +0700

    LongNT:edit st

--- SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/usecase/UpdateAssetRequestUsecase.java
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/usecase/UpdateAssetRequestUsecase.java
@@ -2,12 +2,16 @@ package com.example.swp391_assetmanagement.usecase;
 
 import com.example.swp391_assetmanagement.entity.AssetRequest;
 import com.example.swp391_assetmanagement.enums.RequestStatus;
+import com.example.swp391_assetmanagement.enums.RequestType;
 import com.example.swp391_assetmanagement.service.AssetExternalRequestDetailService;
 import com.example.swp391_assetmanagement.service.AssetRequestService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Objects;
@@ -19,8 +23,16 @@ public class UpdateAssetRequestUsecase {
     private final AssetExternalRequestDetailService assetExternalRequestDetailService;
     private final AssetRequestService assetRequestService;
 
-
+    @Transactional
     public void execute(Long assetRequestId, HttpSession session) {
+
+        // Check type request
+        String assetRequestType = assetRequestService.findRequestTypeById(assetRequestId);
+
+        if (!(ObjectUtils.isEmpty(assetRequestType)
+                || !Objects.equals(RequestType.of(assetRequestType).getValue(), RequestType.PROCUREMENT.getValue()))) {
+            throw new ValidationException();
+        }
 
         Integer count = assetExternalRequestDetailService.countOptionDetail(assetRequestId);
         if(count > 0){
