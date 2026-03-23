3b0b5b70e4ea491d73369c228d8bec5d4edb219c
PhatNguyen12 <phatn7542@gmail.com>
2026-03-23 22:01:18 +0700

    PhatNV: fix comment

--- SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/dto/request/CreateMaintenanceRequestDTORequest.java
+++ SWP391_AssetManagement/src/main/java/com/example/swp391_assetmanagement/dto/request/CreateMaintenanceRequestDTORequest.java
@@ -7,21 +7,13 @@ import lombok.Setter;
 @Setter
 public class CreateMaintenanceRequestDTORequest {
 
-    /** ID của asset được chọn để sửa */
     private Long assetId;
 
-    /** Mô tả lỗi do manager nhập */
     private String issueDescription;
 
-    /** Mức độ ưu tiên: LOW / MEDIUM / HIGH / CRITICAL */
     private String priority;
 
-    /** Ghi chú thêm (tuỳ chọn) */
     private String note;
 
-    /**
-     * true  → Gửi ngay (request_status = APPROVED)
-     * false → Lưu nháp (request_status = DRAFT)
-     */
     private Boolean isSubmitted;
 }
